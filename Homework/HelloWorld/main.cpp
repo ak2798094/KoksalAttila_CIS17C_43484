d88882b6bd10ee0200efaf406efc651b818d6331
Attila Koksal <akoksal@student.rccd.edu>
2022-02-28 13:19:55 -0800

    Signed-off-by: Attila Koksal <akoksal@student.rccd.edu>

---
+++ Homework/HelloWorld/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: Attila Koksal
+ * Created on February 28, 2022, 1:18 PM
+ * Purpose:  CPP Template 
+ *           To be copied for each Assignment Problem
+ */
+
+//System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Math, Science, Universal, Conversions, High Dimensioned Arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map Inputs to Outputs -> Process
+    
+    //Display Inputs/Outputs
+    cout<<"Hello World!";
+    
+    //Exit the Program - Cleanup
+    return 0;
+}

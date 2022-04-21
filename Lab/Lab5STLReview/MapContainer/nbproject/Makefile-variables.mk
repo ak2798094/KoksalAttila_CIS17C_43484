eecad52c2f64a0587555470f1ed6741c2ad5c812
Attila Koksal <akoksal@student.rccd.edu>
2022-04-20 17:58:11 -0700

    Signed-off-by: Attila Koksal <akoksal@student.rccd.edu>

---
+++ Lab/Lab5STLReview/MapContainer/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=mapcontainer
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/mapcontainer
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=mapcontainer.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/mapcontainer.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=mapcontainer
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/mapcontainer
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=mapcontainer.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/mapcontainer.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk

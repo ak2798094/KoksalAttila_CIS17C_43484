3b58d7b6deb2af13cf80b75586f580d15170ff8c
Attila Koksal <akoksal@student.rccd.edu>
2022-06-12 23:00:02 -0700

    Signed-off-by: Attila Koksal <akoksal@student.rccd.edu>

---
+++ Class/Final 17c/FinalProblem5/main.cpp
@@ -0,0 +1,34 @@
+/** 
+ * @file:   main.cpp
+ * @author: Attila Koksal
+ * @date: Created on June 10, 2022, 7:30 PM
+ * Purpose:  Problem 5
+ */
+
+//System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Math, Science, Universal, Conversions, High Dimensioned Arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map Inputs to Outputs -> Process
+    
+    //Display Inputs/Outputs
+    cout<<"Problem 5"<<endl;
+    
+    //Exit the Program - Cleanup
+    return 0;
+}
